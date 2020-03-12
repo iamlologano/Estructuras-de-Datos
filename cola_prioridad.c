833bd7e6102bdb9c34d84d85203aae4a23092e9f
lologano <lologano@Hackiando.Jaimonimous>
2020-03-12 17:27:34 -0500

    Apuntes dia 12 de marzo

---
+++ cola_prioridad.c
@@ -0,0 +1,34 @@
+struct Elemento {
+	int dato;
+	int prioridad;
+};
+struct PriorityTail{
+	struct Elemento Tail[100];
+	int frente;
+	int atras;
+}
+
+int error(int * a){
+if a = -1;
+}
+int atender (struct PriorityTail * Tail int * error) {
+	int i =0;
+	if(Tail == NULL) {
+		*error= -1;
+		return 0;
+	}
+/* caso 1, recorrer la cola que ya esta en orden*/ //recorrido de cola, confuso asf cuando contamos con prioridades, importante empezar con el frente, para respetar la estructura de la cola. 
+       		for(i=Tail->frente; i<=Tail->atras; i--) {
+	
+
+	}
+
+/* caso 2, recorrer la cola por partes*/	
+		for(i= cola ->frente; i>= 0; i--) {
+		}
+int main(){
+	int *error= 0;
+	dato = atender(&Tail,&error);
+	if( error == -1{
+			printf("La funcion atender fallo"\n);
+}
